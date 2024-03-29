a63587edd90156f4d1d6cd2ab302cb5c9a99749d
Diogo <diogoscarvalho02@gmail.com>
2024-03-29 14:09:14 -0300

    Projeto

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+
+import cidadeVerde from './imagens/cidade_verde.jpg'
+
+import logoBranca from './imagens/logo_branca.png'
+import logoVerde from './imagens/logo_verde.png'
+
+import nomeBranco from './imagens/nome_branco.png'
+import nomeVerde from './imagens/nome_verde.png'
+
+function App() {
+  return(
+
+    <div className='App'>
+
+      <div className='cabecalho'>
+
+        <div className='logo_cabecalho'>
+          <img src={logoVerde} className='logo_pequena'></img>
+        </div>
+
+        <div className='botoes_cabecalho'>
+          <button className='botao_entrar'>ENTRAR</button>
+          <button className='botao_registrar'>REGISTRAR</button>
+        </div>
+
+      </div>
+
+      <div className='meio'>
+
+        <div className='letreiro'>
+
+          <div className='nome_letreiro'>
+            <h1 className='nome'>SCOUTER</h1>
+          </div>
+
+          <div className='botao_letreiro'>
+            <button className='botao_saiba'>SAIBA MAIS</button>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className='rodape'>
+        <p className='direitos_rodape1'>SCOUTER® - Marca Registrada</p>
+        <p className='direitos_rodape2'>Copyright © 2024 | scouter.com | TODOS OS DIREITOS RESERVADOS</p>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default App
