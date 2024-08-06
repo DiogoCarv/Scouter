0304b796bad42f4204ef85f35394dcab5fdbfa0a
RogerioPereira22 <rogeriojr2116@gmail.com>
2024-08-06 15:11:18 -0300

    pegando componentes excluidos

---
+++ src/App.jsx
@@ -0,0 +1,138 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+import LoginForm from './components/LoginForm';
+
+import cidadeVerde from './imagens/cidade_verde.jpg'
+
+import logoBranca from './imagens/logo_branca.png'
+import logoVerde from './imagens/logo_verde.png'
+
+import nomeBranco from './imagens/nome_branco.png'
+import nomeVerde from './imagens/nome_verde.png'
+import RegistrationForm from './components/RegistrationForm';
+
+
+function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
+  const handleLoginButtonClick = () => {
+      setShowLogin(true);
+  };
+
+  const handleCloseLogin = () => {
+      setShowLogin(false);
+  };
+
+  const handleRegisterButtonClick = () => {
+    setShowRegister(true);
+};
+
+const handleCloseRegister = () => {
+    setShowRegister(false);
+};
+  return(
+
+    <div className='App'>
+
+      <div className='cabecalho'>
+
+        <div className='logo_cabecalho'>
+          <img src={logoVerde} className='logo_pequena'></img>
+        </div>
+
+        <div className='botoes_cabecalho'>
+            {!showLogin && (
+                <button className='botao_entrar' onClick={handleLoginButtonClick}>ENTRAR</button>
+            )}
+            {showLogin && (
+                <div className="overlay" onClick={handleCloseLogin}>
+                    <div className="modal">
+                        <button className="close-button" onClick={handleCloseLogin}>X</button>
+                        <LoginForm />
+                    </div>
+                </div>
+            )}
+
+          {!showRegister && (
+                <button className='botao_registrar' onClick={handleRegisterButtonClick}>REGISTRAR</button>
+            )}
+            {showRegister && (
+                <div className="overlay" onClick={handleCloseRegister}>
+                    <div className="modal">
+                        <button className="close-button" onClick={handleCloseRegister}>X</button>
+                        <RegistrationForm />
+                    </div>
+                </div>
+            )}
+        </div>
+
+      </div>
+
+      <div className='meio'>
+      
+          <div className='letreiro'>
+
+            <div className='nome_letreiro'>
+                <h1 className='nome'>SCOUTER</h1>
+                
+            </div>  
+            <div class="centro_botao">
+            <i className="icon icon-angle-down seta"></i>
+            </div>
+            <div class="centro_botao">
+              
+              <button class="botao_saiba">Saiba Mais</button>
+              
+           </div>
+          </div>
+
+       
+
+        <div className='explicacao'>
+
+            <div className='esquerda'>
+              <p className='titulo1'>MOTIVO</p>
+             
+            </div>
+           
+            <div className='direita'>
+              <p className='texto1'>Bem-vindo a plataforma onde os cidadãos podem reportar problemas urbanos diretamente para os órgãos competentes. Nosso objetivo é melhorar a qualidade de vida em nossa cidade, facilitando a comunicação entre a população e as autoridades responsáveis pela manutenção e desenvolvimento urbano.,<br></br> assim avise os orgãos responsáveis e os outros moradores.</p>
+            </div>
+
+        </div>
+
+        <div className='faixa_quadrado'>
+
+          <div className='quadrado'>
+
+            <div className='esquerda_quadrado'>
+              <p className='titulo2'>Envie suas reclamações com fotos no sistema</p>
+            </div>
+
+            <div className='direita_quadrado'>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      
+
+      <div className='rodape'>
+        <p className='direitos_rodape1'>SCOUTER® - Marca Registrada</p>
+        <p className='direitos_rodape2'>Copyright © 2024 | scouter.com | TODOS OS DIREITOS RESERVADOS</p>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default App
