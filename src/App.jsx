e6e3038dde6933911546282dbe98b37c5fae9f89
Diogo <diogoscarvalho02@gmail.com>
2024-08-07 23:01:58 -0300

    Modificações no front

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,107 @@
+//Importações já vindas
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+//----------------------------------------
+
+// Importações CSS
+
 import './App.css'
-import LoginForm from './components/LoginForm';
-
 import cidadeVerde from './imagens/cidade_verde.jpg'
-
 import logoBranca from './imagens/logo_branca.png'
 import logoVerde from './imagens/logo_verde.png'
 
-import nomeBranco from './imagens/nome_branco.png'
-import nomeVerde from './imagens/nome_verde.png'
+//----------------------------------------
+
+// Importações Login e Registrar
+
 import RegistrationForm from './components/RegistrationForm';
+import LoginForm from './components/LoginForm';
+
+//----------------------------------------
+
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import Divider from '@mui/material/Divider';
+
+const images = [
+  {
+    url: './imagens/cidade_verde.jpg',
+    title: 'SAIBA MAIS',
+    width: '30%',
+  },
+];
+
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  height: 200,
+  [theme.breakpoints.down('sm')]: {
+    width: '100% !important', // Overrides inline-style
+    height: 100,
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiImageBackdrop-root': {
+      opacity: 0.15,
+    },
+    '& .MuiImageMarked-root': {
+      opacity: 0,
+    },
+    '& .MuiTypography-root': {
+      border: '4px solid currentColor',
+    },
+  },
+}));
+
+const ImageSrc = styled('span')({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center 40%',
+});
+
+const Image = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity'),
+}));
+
+const ImageMarked = styled('span')(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity'),
+}));
 
 
 function App() {
@@ -76,32 +165,56 @@ const handleCloseRegister = () => {
       
           <div className='letreiro'>
 
-            <div className='nome_letreiro'>
-                <h1 className='nome'>SCOUTER</h1>
-                
-            </div>  
-            <div class="centro_botao">
-            <i className="icon icon-angle-down seta"></i>
-            </div>
-            <div class="centro_botao">
-              
-              <button class="botao_saiba">Saiba Mais</button>
-              
-           </div>
-          </div>
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
+              {images.map((image) => (
+                <ImageButton
+                  focusRipple
+                  key={image.title}
+                  style={{
+                    width: image.width,
+                  }}
+                >
+                  <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+                  <ImageBackdrop className="MuiImageBackdrop-root" />
+                  <Image>
+                    <Typography
+                      component="span"
+                      variant="subtitle1"
+                      color="inherit"
+                      sx={{
+                        position: 'relative',
+                        p: 4,
+                        pt: 2,
+                        pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                      }}
+                    >
+                      {image.title}
+                      <ImageMarked className="MuiImageMarked-root" />
+                    </Typography>
+                  </Image>
+                </ImageButton>
+              ))}
+            </Box>
 
-       
+          
+          </div>
 
         <div className='explicacao'>
 
-            <div className='esquerda'>
-              <p className='titulo1'>MOTIVO</p>
-             
-            </div>
-           
-            <div className='direita'>
-              <p className='texto1'>Bem-vindo a plataforma onde os cidadãos podem reportar problemas urbanos diretamente para os órgãos competentes. Nosso objetivo é melhorar a qualidade de vida em nossa cidade, facilitando a comunicação entre a população e as autoridades responsáveis pela manutenção e desenvolvimento urbano.,<br></br> assim avise os orgãos responsáveis e os outros moradores.</p>
-            </div>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              bgcolor: 'background.paper',
+              color: 'text.secondary',
+              '& svg': {
+                m: 1,
+              },
+            }}
+          >
+            <h1 className='titulo1'>Motivo</h1>
+            <p className='texto1'>Bem-vindo a plataforma onde os cidadãos podem reportar problemas urbanos diretamente para os órgãos competentes. Nosso objetivo é melhorar a qualidade de vida em nossa cidade, facilitando a comunicação entre a população e as autoridades responsáveis pela manutenção e desenvolvimento urbano, assim avise os orgãos responsáveis e os outros moradores.</p>
+          </Box>
 
         </div>
 
@@ -123,11 +236,26 @@ const handleCloseRegister = () => {
 
       </div>
 
-      
-
       <div className='rodape'>
-        <p className='direitos_rodape1'>SCOUTER® - Marca Registrada</p>
-        <p className='direitos_rodape2'>Copyright © 2024 | scouter.com | TODOS OS DIREITOS RESERVADOS</p>
+
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            border: '1px solid',
+            borderColor: 'divider',
+            borderRadius: 2,
+            bgcolor: 'background.paper',
+            color: 'text.secondary',
+            '& svg': {
+              m: 1,
+            },
+          }}
+        >
+          <p className='direitos_rodape1'>SCOUTER® - Marca Registrada</p>
+          <p className='direitos_rodape2'>Copyright © 2024 | scouter.com | TODOS OS DIREITOS RESERVADOS</p>
+        </Box>
+
       </div>
 
     </div>
