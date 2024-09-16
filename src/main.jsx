541a3ae355e80d111abf76bc8465ebe289cd4d14
Diogo <diogoscarvalho02@gmail.com>
2024-09-16 13:36:48 -0300

    Voltando uma parte

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
-import Login from './Login.jsx'; // Corrija para importação do componente correto
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   </React.StrictMode>
