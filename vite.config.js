2a7605b6fbbfe23283253905b8329a3f4310feb7
RogerioPereira22 <rogeriojr2116@gmail.com>
2024-09-20 14:56:28 -0300

    removi js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    loader: 'jsx', // Isso garante que os arquivos .js que contenham JSX sejam interpretados corretamente
+    include: /src\/.*\.js$/, // Inclua arquivos .js dentro da pasta src
+    exclude: /node_modules/, // Exclua os node_modules
+  },
+});
