274d7472a2296ed14c8f615baacff7edaf34b223
Diogo Carvalho <diogoscarvalho02@gmail.com>
2024-09-26 18:53:14 -0300

    Correção Criar Orgão

--- src/components/CriarOrgao.jsx
+++ src/components/CriarOrgao.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
+import './CriarOrgao.css';
 
 const CriarOrgao = () => {
   const [nome, setNome] = useState('');
@@ -44,28 +45,49 @@ const CriarOrgao = () => {
 
   return (
     <div>
-      <h1>Criar Órgão Responsável</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Nome:</label>
-          <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} required />
+
+      <div className='cabecalho'>
+        <img src="https://i.ibb.co/vJRNYqQ/logo-verde.png" className="App-logo" alt="logo" />
+      </div>
+
+      <div className='principal'>
+
+        <div className='bloco'>
+          
+          <h1 className='titulo'>CRIAR ORGÃO RESPONSÁVEL</h1>
+
+          <form onSubmit={handleSubmit}>
+
+            <div className='preencher'>
+              <label>NOME:</label>
+              <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} required />
+            </div>
+
+            <div className='preencher'>
+              <label>EMAIL:</label>
+              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            </div>
+
+            <div className='preencher'>
+              <label>SENHA:</label>
+              <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} required />
+            </div>
+
+            <div className='preencher'>
+              <label>TELEFONE:</label>
+              <input type="telefone" value={telefone} onChange={(e) => setTelefone(e.target.value)} required />
+            </div>
+
+            {errorMessage && <p className="error">{errorMessage}</p>}
+            {successMessage && <p className="success">{successMessage}</p>}
+
+            <button type="submit" className='botao'>CRIAR ORGÃO</button>
+
+          </form>
         </div>
-        <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        </div>
-        <div>
-          <label>Senha:</label>
-          <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} required />
-        </div>
-        <div>
-          <label>Telefone:</label>
-          <input type="telefone" value={telefone} onChange={(e) => setTelefone(e.target.value)} required />
-        </div>
-        {errorMessage && <p className="error">{errorMessage}</p>}
-        {successMessage && <p className="success">{successMessage}</p>}
-        <button type="submit">Criar Órgão</button>
-      </form>
+
+      </div>
+
     </div>
   );
 };
