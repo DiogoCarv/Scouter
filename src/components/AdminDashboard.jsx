4ffee50b7f2178104b858eb9c8b0c230554f5c35
Diogo Carvalho <diogoscarvalho02@gmail.com>
2024-09-26 18:23:33 -0300

    Correções Admin Dashboard

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -2,15 +2,33 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './AdminDashboard.css';
 
 const AdminDashboard = () => {
   return (
     <div>
-      <h1>Painel do Administrador</h1>
-      <ul>
-        <li><Link to="/criar-orgao">Criar Órgão Responsável</Link></li>
-        <li><Link to="/listar-todos-problemas">Listar Todos os Problemas</Link></li>
-      </ul>
+
+      <div className='cabecalho'>
+        <img src="https://i.ibb.co/vJRNYqQ/logo-verde.png" className="App-logo" alt="logo" />
+      </div>
+
+      <div className='principal'>
+
+        <div className='bloco'>
+
+          <h1 className='titulo'>PAINEL ADMINISTRADOR</h1>
+
+          <div className='botoes'>
+
+            <button className='botao'><Link to="/criar-orgao" className='conteudo'>CRIAR ORGÃO RESPONSÁVEL</Link></button>
+            <button className='botao'><Link to="/listar-todos-problemas" className='conteudo'>LISTAR PROBLEMAS</Link></button>
+
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 };
