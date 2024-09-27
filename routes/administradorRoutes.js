0bc04aaaa370a3586d5a62d6a96cd5c72c1c106f
RogerioPereira22 <rogeriojr2116@gmail.com>
2024-09-26 22:16:55 -0300

    arrumando rotas

--- routes/administradorRoutes.js
+++ routes/administradorRoutes.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import {listarProblemas} from '../controllers/problemaController.js';
 import {criarOrgao} from '../controllers/orgaoCompetenteController.js';
 const router = express.Router();
 
 
 router.post('/criar-orgao', criarOrgao);
-router.get('/listar-problemas-orgao',listarProblemas);
 
 export default router;
--- routes/problemaRoutes.js
+++ routes/problemaRoutes.js
@@ -2,10 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 
-import { listarProblemas, obterProblema, criarProblema, atualizarProblema, excluirProblema } from '../controllers/problemaController.js';
+import { listarProblemas, criarProblema, atualizarProblema, excluirProblema } from '../controllers/problemaController.js';
 
 router.get('/listar-problemas', listarProblemas);
-router.get('/obter-problemas', obterProblema);
 router.post('/criar-problemas', criarProblema);
 router.put('/atualizar-problemas', atualizarProblema);
 router.delete('/excluir-problemas', excluirProblema);
